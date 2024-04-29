f4ca8f4c0f63b73ab86a1c22054e9d4630f8a17c
Oleksandr Rohan <o.rohanov@gmail.com>
2024-04-29 13:46:38 +0300

    Initial commit. Add feature User

---
+++ ProjectToTest/User.cs
@@ -0,0 +1,13 @@
+﻿namespace ProjectToTest
+{
+    internal class User
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public User(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+    }
+}
