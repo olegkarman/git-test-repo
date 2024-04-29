8b0a70d1a1cb9b9256dc87aaa74609bba676b57a
Oleksandr Rohan <o.rohanov@gmail.com>
2024-04-29 13:55:37 +0300

    Add properties Age

--- ProjectToTest/User.cs
+++ ProjectToTest/User.cs
@@ -4,10 +4,12 @@
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public User(string firstName, string lastName)
+        public int Age { get; set; }
+        public User(string firstName, string lastName, int age)
         {
             FirstName = firstName;
             LastName = lastName;
+            Age = age;
         }
     }
 }
